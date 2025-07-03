833659029cbe499c8c68b6ad2d96447c480caaa6
Vansh Nyati <vansh.nyati@learner.manipal.edu>
2025-07-03 23:08:41 +0530

    Initial project setup and  Folder structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center mt-10 text-3xl font-bold text-blue-600">
+      🚀 Flam HR Dashboard Assignment
+    </div>
+  );
+}
+
+export default App;
+
